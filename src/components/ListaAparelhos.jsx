21198bb3a81cb64799ff916bdbb37e54b78d8c4a
amandabomfim <rm99741@fiap.com.br>
2023-09-05 16:24:17 -0300

    Adicionado foto e descrição.

--- src/components/ListaAparelhos.jsx
+++ src/components/ListaAparelhos.jsx
@@ -1,9 +1,9 @@
 export const ListaAparelhos = [
-    {id:1, nome: 'Samsung Galaxy S23', preco: 3704,},
-    {id:2, nome: 'Smartphone Xiaomi Redmi Note 12', preco: 1727},
-    {id:3, nome: 'Apple iPhone 14', preco: 12221},
-    {id:4, nome: 'Tablet Samsung S7 FE', preco: 2719},
-    {id:5, nome: 'Apple iPad Pro', preco: 10999},
+    {id:1, nome: 'Samsung Galaxy S23', preco: 3704, foto: '../images/samsung.jpg',desc: 'Cor Branco, 8GB RAM'},
+    {id:2, nome: 'Smartphone Xiaomi Redmi Note 12', preco: 1727,foto: '../images/xiaomi.jpg', desc: 'Cor Preto, 4GB RAM'},
+    {id:3, nome: 'Apple iPhone 14', preco: 12221, foto: '../images/iphone.jpg', desc: ' Cor Roxo, 6 GB RAM'},
+    {id:4, nome: 'Tablet Samsung S7 FE', preco: 2719, foto: '../images/tablet.jpg',desc: 'Cor Preto, 6 GB RAM '},
+    {id:5, nome: 'Apple iPad Pro', preco: 10999, foto: '../images/ipad.jpg', desc: 'Cor Preto, 8GB RAM'},
    
 
 
